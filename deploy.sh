7f17e90ecc740a2192c2268644d6bb40f584b064
ayush <58028488+desibabua@users.noreply.github.com>
2020-10-01 10:45:04 +0530

    modified deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,6 @@ rm -rf ./public
 
 echo "Started clonning..."
 git clone https://github.com/desibabua/E-buddy.git 2> /dev/null
-echo "Clonning completed..."
 
 echo "Installing dependencies..."
 cd E-buddy
@@ -18,4 +17,4 @@ cd ..
 echo "Started merging..."
 mkdir public
 cp -r ./E-buddy/build/* ./public 
-rm -rf E-buddy        
+rm -rf E-buddy
