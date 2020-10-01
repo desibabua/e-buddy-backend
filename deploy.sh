feef004853857f2c4bab5d5a8b10aac602e240c7
ayush <58028488+desibabua@users.noreply.github.com>
2020-10-01 11:45:27 +0530

    modified deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,37 +1,24 @@
 #! /bin/bash
 
-rm -rf ${BACKEND}
-echo 'Cloning backend...'
-git clone  https://github.com/${USERNAME}/${BACKEND}.git 2> /dev/null
-cd ${BACKEND}
+rm -rf ./public
 
-echo 'Installing dependencies...'
-npm install 2> /dev/null
-npm test
+echo "Started clonning..."
+git clone https://github.com/desibabua/E-buddy.git 2> /dev/null
 
+echo "Installing dependencies..."
+cd E-buddy
+npm install
+npm run test
+
+echo "Creating build..."
+npm run build
 
 cd ..
-rm -rf ${FRONTEND}
-echo 'Cloning frontend...'
-git clone https://github.com/${USERNAME}/${FRONTEND}.git 2> /dev/null
-cd ${FRONTEND}
+echo "Started merging..."
+rm -rf public
+mkdir public
+cp -r ./E-buddy/build/* ./public
 
-echo 'Installing dependencies...'
-npm install 2> /dev/null
-npm test
-
-echo 'Creating build ...'
-npm run build 2> /dev/null
-
-echo 'Merging ...'
-mkdir -p ../public
-mv build/* ../public/.
-
-echo 'Removing un-neccessaries...'
-cd ../${BACKEND}
-rm .travis.yml
-cd ..
-rm -rf ${FRONTEND}
-mv ${BACKEND}/* ${BACKEND}/.* .
-rm -rf ${BACKEND}
-echo 'Build created...'
+echo "removing un-neccessary file"
+rm -rf node_modules
+rm -rf E-buddy
