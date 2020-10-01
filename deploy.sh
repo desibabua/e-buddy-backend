f61e1d07c566e407d779850c0532babc94d3af97
ayush <58028488+desibabua@users.noreply.github.com>
2020-10-01 10:35:01 +0530

    modified deploy automatic

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,20 @@
 #! /bin/bash
 
-echo "before build"
+rm -rf ./public
+
+echo "Started clonning..."
+git clone https://github.com/desibabua/E-buddy.git 2> /dev/null
+echo "Clonning completed..."
+
+echo "Installing dependencies..."
+cd E-buddy
+npm install
+npm test
+
+echo "Creating build..."
+npm run build
+
+cd ..
+echo "Started merging..."
+mkdir public
+cp -r ./E-buddy/build/* ./public       
