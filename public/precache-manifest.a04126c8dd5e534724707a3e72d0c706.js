19a076897004ec7ec474d72c9fd55e74da4e0599
Ayush kumar <kumarayush074@gmail.com>
2023-03-13 16:38:48 +0530

    merge build

---
+++ public/precache-manifest.a04126c8dd5e534724707a3e72d0c706.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd31550ffc4926ec75dba9bd7b05eea1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6084d6c54566110fd2d",
+    "url": "/static/css/main.496e493b.chunk.css"
+  },
+  {
+    "revision": "0b92b2b48d781624a8b3",
+    "url": "/static/js/2.91fe54c2.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/static/js/2.91fe54c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6084d6c54566110fd2d",
+    "url": "/static/js/main.84f8f145.chunk.js"
+  },
+  {
+    "revision": "7e18954f0cc50346ae58",
+    "url": "/static/js/runtime-main.9c1ad282.js"
+  },
+  {
+    "revision": "422a57a854e93518aed7c781ec0deeb1",
+    "url": "/static/media/e-buddy-logo.422a57a8.svg"
+  }
+]);
