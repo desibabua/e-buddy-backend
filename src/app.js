13737c1259a4ac6e2b0e39c0deacabe5dcc7159c
Ayush kumar <kumarayush074@gmail.com>
2023-03-13 15:54:14 +0530

    added data for home page ads and passed the same

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ const express = require('express');
 
 const cookieParser = require('cookie-parser');
 const { Sessions } = require('../lib/sessions');
-const homeDetails = require('../data/home.json');
+// const homeDetails = require('../data/home.json');
+const homeAds = require('../data/sponsoredAds.json');
 
 const { register, login, logout } = require('./oAuth');
 const {
@@ -25,7 +26,6 @@ app.locals.sessions = new Sessions();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static('public'));
 app.use((req, res, next) => {
   console.log(req.method, req.url);
   next();
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
 app.get('/api/login', login);
 app.get('/api/logout', logout);
 app.get('/api/register', register);
-app.get('/api/home', (req, res) => res.json(homeDetails));
+app.get('/api/home', (req, res) => res.json(homeAds));
 app.get('/api/getUser', getUser);
 app.get('/api/search', getSearchedProducts);
 app.get('/api/product/:id', getProduct);
@@ -42,10 +42,12 @@ app.get('/api/products/:category', getProducts);
 app.get('/api/sponsoredProducts/:category', getSponsoredProducts);
 app.get('/api/review/:id', getProductReviews);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../public/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/../public/index.html'));
+// });
 
 app.post('/api/addReview', addReview);
+app.use(express.static('public'));
+
 
 module.exports = { app };
